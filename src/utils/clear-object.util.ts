19e632b8d6945bf7a9b0019e34e05cb4bbf4f787
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-01-27 00:51:51 -0300

    feat: update clearObject utils

--- src/utils/clear-object.util.ts
+++ src/utils/clear-object.util.ts
@@ -1,11 +1,19 @@
+import { isRegExp } from 'util/types'
+
 export function clearObject <T> (obj: any): T {
   const result: any = {}
 
   Object.keys(obj).forEach(key => {
     if (obj[key] !== undefined && obj[key] !== '') {
-      result[key] = obj[key]
-    } else if (typeof obj[key] === 'object') {
-      result[key] = clearObject(obj[key])
+      if (typeof obj[key] === 'object' && !Array.isArray(obj[key]) && !isRegExp(obj[key])) {
+        const r = clearObject<any>(obj[key])
+
+        if (Object.keys(r).length > 0) {
+          result[key] = r
+        }
+      } else {
+        result[key] = obj[key]
+      }
     }
   })
 
