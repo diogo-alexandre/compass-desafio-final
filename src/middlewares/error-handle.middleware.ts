4851a85ec9a152944851c96f01aaf66f5dec77b4
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-01-27 23:45:03 -0300

    ref: error handler responds with message

--- src/middlewares/error-handle.middleware.ts
+++ src/middlewares/error-handle.middleware.ts
@@ -1,16 +1,26 @@
 import { ErrorRequestHandler } from 'express'
-import { HttpCode } from '../constants/http-code.contant'
-import { HttpError } from '../errors/http/http.error'
+
 import { Log } from '../helpers/log.helper'
+import { HttpError } from '../errors/http/http.error'
+import { HttpCode } from '../constants/http-code.contant'
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  let statusCode: HttpCode = HttpCode.INTERNAL_SERVER_ERROR
-
-  if (err instanceof HttpError) {
-    statusCode = err.statusCode
+  const result = {
+    name: 'Internal Server Error',
+    statusCode: HttpCode.INTERNAL_SERVER_ERROR,
+    details: [{ message: 'Unexpected Internal Error' }]
   }
 
-  if (statusCode === HttpCode.INTERNAL_SERVER_ERROR) Log.error(err)
+  if (err instanceof HttpError) {
+    result.name = err.name
+    result.statusCode = err.statusCode
+    result.details[0] = { message: err.message }
+  }
 
-  return res.status(statusCode).end()
+  if (result.statusCode === HttpCode.INTERNAL_SERVER_ERROR) Log.error(err)
+
+  return res.status(result.statusCode).json({
+    name: result.name,
+    details: result.details
+  }).end()
 }
