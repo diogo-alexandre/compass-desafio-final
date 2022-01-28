40e952694cc39af428037a580e973db4b0c5640f
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-01-28 10:58:21 -0300

    ref: people create endpoint throw conflict error on duplicated entries

--- src/controllers/people.controller.ts
+++ src/controllers/people.controller.ts
@@ -2,6 +2,8 @@ import { Inject } from '@decorators/di'
 import { Controller, Post } from '@decorators/express'
 import { Request, NextFunction } from 'express'
 import { HttpCode } from '../constants/http-code.contant'
+import { DuplicatedEntry } from '../errors/duplicated-entry.error'
+import { Conflict } from '../errors/http/conflict.error'
 import { IPeopleDTO } from '../helpers/interfaces/people.interface'
 import { Response } from '../helpers/interfaces/response.interface'
 import { IPeopleService } from '../services/interfaces/people-service.interface'
@@ -23,7 +25,13 @@ export class PeopleController {
 
       return res.status(HttpCode.CREATED).end()
     } catch (err) {
-      return next(err)
+      let localError = err
+
+      if (err instanceof DuplicatedEntry) {
+        localError = new Conflict(err.message)
+      }
+
+      return next(localError)
     }
   }
 }
--- src/services/people.service.ts
+++ src/services/people.service.ts
@@ -1,11 +1,12 @@
-import { Inject, Injectable } from '@decorators/di'
 import bcrypt from 'bcrypt'
-import { EntityNotFound } from '../errors/entity-not-found.error'
+import { Inject, Injectable } from '@decorators/di'
 
-import { IPeople, IPeopleDTO } from '../helpers/interfaces/people.interface'
-import { IPeopleRepository } from '../repositories/interfaces/people-repository.interface'
+import { EntityNotFound } from '../errors/entity-not-found.error'
+import { DuplicatedEntry } from '../errors/duplicated-entry.error'
 import { PeopleRepository } from '../repositories/people.repository'
 import { IPeopleService } from './interfaces/people-service.interface'
+import { IPeople, IPeopleDTO } from '../helpers/interfaces/people.interface'
+import { IPeopleRepository } from '../repositories/interfaces/people-repository.interface'
 
 @Injectable()
 export class PeopleService implements IPeopleService {
@@ -15,14 +16,26 @@ export class PeopleService implements IPeopleService {
   ) { }
 
   async create (people: IPeopleDTO): Promise<IPeople> {
-    return await this.peopleRepository.create({
-      nome: people.nome,
-      cpf: people.cpf,
-      data_nascimento: people.data_nascimento,
-      email: people.email,
-      senha: await bcrypt.hash(people.senha, 8),
-      habilitado: (people.habilitado === 'sim')
-    })
+    try {
+      return await this.peopleRepository.create({
+        nome: people.nome,
+        cpf: people.cpf,
+        data_nascimento: people.data_nascimento,
+        email: people.email,
+        senha: await bcrypt.hash(people.senha, 8),
+        habilitado: (people.habilitado === 'sim')
+      })
+    } catch (err) {
+      let localError: any = err
+
+      if (localError.name === 'MongoServerError' && localError.code === 11000) {
+        const key = Object.keys(localError.keyPattern)[0]
+
+        localError = new DuplicatedEntry(`Already exists People with same value of ${key}`)
+      }
+
+      throw localError
+    }
   }
 
   async findByEmail (email: string): Promise<IPeople> {
