1b144b6292edc24cc363741ef07bbb44dc7dc51e
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-01-28 01:52:53 -0300

    ref: add injectable decorator to people repository

--- src/repositories/people.repository.ts
+++ src/repositories/people.repository.ts
@@ -1,7 +1,9 @@
 import { People } from '../schemas/people.schema'
 import { IPeople } from '../helpers/interfaces/people.interface'
 import { IPeopleRepository } from './interfaces/people-repository.interface'
+import { Injectable } from '@decorators/di'
 
+@Injectable()
 export class PeopleRepository implements IPeopleRepository {
   async create (people: IPeople): Promise<IPeople> {
     return await People.create(people)
