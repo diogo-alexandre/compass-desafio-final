cec8299f95a779c95775be8e1d17e8255b7d73e1
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-02-02 11:28:41 -0300

    fix: clearObject dont ignores date objects

--- src/utils/clear-object.util.ts
+++ src/utils/clear-object.util.ts
@@ -3,12 +3,10 @@ import { isRegExp } from 'util/types'
 export function clearObject <T> (obj: any): T {
   const result: any = {}
 
-  console.log(obj)
-
   Object.keys(obj).forEach(key => {
     if (!(isNaN(obj[key]) && typeof obj[key] === 'number')) {
       if (obj[key] !== undefined && obj[key] !== '') {
-        if (typeof obj[key] === 'object' && !Array.isArray(obj[key]) && !isRegExp(obj[key])) {
+        if (typeof obj[key] === 'object' && !Array.isArray(obj[key]) && !isRegExp(obj[key]) && !(obj[key] instanceof Date)) {
           const r = clearObject<any>(obj[key])
 
           if (Object.keys(r).length > 0) {
