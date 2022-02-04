c2c09a618cc3c0d3618418547116d15e089832d9
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-02-04 09:20:56 -0300

    test: add test for endpoint post people

--- __tests__/e2e/app.test.ts
+++ __tests__/e2e/app.test.ts
@@ -108,4 +108,132 @@ describe('Feature Test', () => {
       })
     })
   })
+
+  describe('/api/v1/people', () => {
+    describe('POST - create a user', () => {
+      const prefix = '/api/v1/people'
+
+      it('should throw "bad request" when request without body request', async () => {
+        const res = await supertest(app).post(prefix).send({})
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request without required field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          cpf: '86416348004',
+          data_nascimento: '23/02/2002',
+          email: 'valid-email@gmail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "cpf" field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: 'invalid-cpf-field',
+          data_nascimento: '23/02/2002',
+          email: 'valid-email@gmail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "data_nascimento" field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: 'invalid-date',
+          email: 'valid-email@gmail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "data_nascimento" date format field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: '2002/02/23',
+          email: 'valid-email@gmail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "email" field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: '23/02/2002',
+          email: 'invalid-email',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "senha" field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: '23/02/2002',
+          email: 'valid-email@mail.com',
+          senha: 'inval',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "bad request" when request with invalid "habilitado" field', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: '23/02/2002',
+          email: 'valid-email@mail.com',
+          senha: 'valid-password',
+          habilitado: 'invalid-option'
+        })
+
+        expect(res.statusCode).toBe(400)
+      })
+
+      it('should throw "conflict" when request with fields that already exists on app', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '68206259015',
+          data_nascimento: '23/02/2002',
+          email: 'other-valid-email@mail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(409)
+      })
+
+      it('should throw "OK" when request with request body correctly', async () => {
+        const res = await supertest(app).post(prefix).send({
+          nome: 'valid-name',
+          cpf: '86416348004',
+          data_nascimento: '23/02/2002',
+          email: 'valid-email@mail.com',
+          senha: 'valid-password',
+          habilitado: 'sim'
+        })
+
+        expect(res.statusCode).toBe(201)
+      })
+    })
+  })
 })
