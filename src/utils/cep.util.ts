5bab2713864cad90b30d20abf145b9099b407225
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-02-09 23:47:37 -0300

    feat: add cep request util method

---
+++ src/utils/cep.util.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+import { IAdress } from '../helpers/interfaces/entities/adress.interface'
+
+export const CEP = {
+  getAdress: async (cep: string): Promise<Omit<IAdress, 'number'>> => {
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json`)
+    const { logradouro, bairro, localidade } = response.data
+
+    return {
+      cep,
+      logradouro,
+      bairro,
+      localidade
+    }
+  }
+}
