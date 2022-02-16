849ea9b195805e58bfc9a7dda496792c4bc4b441
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-02-16 01:00:42 -0300

    ref: beatify code of test seeders

--- __tests__/e2e/seeders/car.seeder.ts
+++ __tests__/e2e/seeders/car.seeder.ts
@@ -1,42 +1,37 @@
-import { ICarDTO } from '../../../src/helpers/interfaces/entities/car.interface'
+import { ICar } from '../../../src/helpers/interfaces/entities/car.interface'
 import { Car } from '../../../src/schemas/car.schema'
 
 export const CarSeeder = {
-  handle: async (): Promise<ICarDTO[]> => {
-    const entities: ICarDTO[] = []
-
-    entities.push(await Car.create({
-      modelo: 'GM S10 2.8',
-      cor: 'branco',
-      ano: '2021',
-      acessorios: [{ descricao: 'Ar-condicionado' }],
-      quantidadePassageiros: 5
-    }))
-
-    entities.push(await Car.create({
-      modelo: 'VW GOL 1.0',
-      cor: 'branco',
-      ano: '2015',
-      acessorios: [{ descricao: 'Ar-condicionado' }],
-      quantidadePassageiros: 5
-    }))
-
-    entities.push(await Car.create({
-      modelo: 'VW Fusca',
-      cor: 'azul',
-      ano: '1950',
-      acessorios: [{ descricao: 'Funciona de baixo d\'agua' }],
-      quantidadePassageiros: 5
-    }))
-
-    entities.push(await Car.create({
-      modelo: 'VW Combi',
-      cor: 'preto',
-      ano: '1950',
-      acessorios: [{ descricao: 'Espaçoso' }],
-      quantidadePassageiros: 8
-    }))
-
-    return entities
+  handle: async (): Promise<ICar[]> => {
+    return [
+      await Car.create({
+        modelo: 'GM S10 2.8',
+        cor: 'branco',
+        ano: '2021',
+        acessorios: [{ descricao: 'Ar-condicionado' }],
+        quantidadePassageiros: 5
+      }),
+      await Car.create({
+        modelo: 'VW GOL 1.0',
+        cor: 'branco',
+        ano: '2015',
+        acessorios: [{ descricao: 'Ar-condicionado' }],
+        quantidadePassageiros: 5
+      }),
+      await Car.create({
+        modelo: 'VW Fusca',
+        cor: 'azul',
+        ano: '1950',
+        acessorios: [{ descricao: 'Funciona de baixo d\'agua' }],
+        quantidadePassageiros: 5
+      }),
+      await Car.create({
+        modelo: 'VW Combi',
+        cor: 'preto',
+        ano: '1950',
+        acessorios: [{ descricao: 'Espaçoso' }],
+        quantidadePassageiros: 8
+      })
+    ]
   }
 }
--- __tests__/e2e/seeders/people.seeder.ts
+++ __tests__/e2e/seeders/people.seeder.ts
@@ -1,14 +1,17 @@
+import { IPeople } from '../../../src/helpers/interfaces/entities/people.interface'
 import { People } from '../../../src/schemas/people.schema'
 
 export const PeopleSeeder = {
-  handle: async () => {
-    await People.create({
-      nome: 'Diogo Alexandre',
-      cpf: '68206259015',
-      data_nascimento: new Date('2002-02-23'),
-      email: 'other-valid-email@mail.com',
-      senha: '$2a$08$n70.qf8BdMqvn78pMWWpG.FKGH2aW4KbmOC6J5SRBqVxtocZYqNTy',
-      habilitado: true
-    })
+  handle: async (): Promise<IPeople[]> => {
+    return [
+      await People.create({
+        nome: 'Diogo Alexandre',
+        cpf: '68206259015',
+        data_nascimento: new Date('2002-02-23'),
+        email: 'other-valid-email@mail.com',
+        senha: '$2a$08$n70.qf8BdMqvn78pMWWpG.FKGH2aW4KbmOC6J5SRBqVxtocZYqNTy',
+        habilitado: true
+      })
+    ]
   }
 }
