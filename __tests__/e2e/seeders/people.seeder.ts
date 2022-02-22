d6fb7708ab5f9187c0334cf14fead9ce4d12c8d1
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2022-02-22 17:32:33 -0300

    test: add other people entity

--- __tests__/e2e/seeders/people.seeder.ts
+++ __tests__/e2e/seeders/people.seeder.ts
@@ -9,7 +9,15 @@ const PeopleSeeder = {
       data_nascimento: new Date('2002-02-23'),
       email: 'other-valid-email@mail.com',
       senha: '$2a$08$n70.qf8BdMqvn78pMWWpG.FKGH2aW4KbmOC6J5SRBqVxtocZYqNTy',
-      habilitado: true,
+      habilitado: 'sim',
+    }),
+    await People.create({
+      nome: 'Joazinho',
+      cpf: '01374993026',
+      data_nascimento: new Date('1992-02-23'),
+      email: 'joazinho@mail.com',
+      senha: '$2a$08$n70.qf8BdMqvn78pMWWpG.FKGH2aW4KbmOC6J5SRBqVxtocZYqNTy',
+      habilitado: 'nao',
     }),
   ],
 };
